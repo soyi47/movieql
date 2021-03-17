45dcd6b7c9c962494c2eada2137c7cc7de60d39c
SoYi Jeon <soyijeon47@gmail.com>
2021-03-17 14:04:51 +0900

    getMovie

--- graphql/DB.js
+++ graphql/DB.js
@@ -1,9 +1,12 @@
 import fetch from "node-fetch";
-const API_URL = "https://yts.am/api/v2/list_movies.json?";
+
+const BASE_URL = "https://yts.am/api/v2/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json?`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json?`;
 
 export const getMovies = (limit, rating) => {
-    let REQUEST_URL = API_URL;
-    if (limit > 0) {
+    let REQUEST_URL = LIST_MOVIES_URL;
+    if (limit > 0) {    // limit 지정 없는 경우 default 값은 20 (https://yts.mx/api#list_movies)
         REQUEST_URL += `limit=${limit}`;
     }
     if (rating > 0) {
@@ -13,3 +16,17 @@ export const getMovies = (limit, rating) => {
         .then(res => res.json())
         .then(json => json.data.movies);
 }
+
+export const getMovie = ( id ) => {
+    let REQUEST_URL = MOVIE_DETAILS_URL;
+    REQUEST_URL += `movie_id=${id}`;
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(json => json.data.movie);
+}
+
+/*
+    < 참고 링크 >
+    https://yts.mx/api 
+    https://yts.mx/api#movie_details
+*/
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,11 @@
-import { getMovies } from './DB';
+import { getMovies, getMovie, getSuggestions } from "./DB";
 
 const resolvers = {
-    Query: {
-        movies: (_, { rating, limit }) => getMovies(limit, rating)
-    }
+  Query: {
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id)
+  }
 };
 
 export default resolvers;
